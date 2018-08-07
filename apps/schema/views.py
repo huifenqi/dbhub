e5a4412a46bab391cf04eadf6e71d8bea1ce53cb
Bin <iamsk.info@gmail.com>
2018-08-07 21:42:33 +0800

    update style

--- apps/schema/views.py
+++ apps/schema/views.py
@@ -1,5 +1,6 @@
 from django_filters.views import FilterView
 from django_tables2 import SingleTableMixin
+from django.conf import settings
 
 from .tables import ColumnTable
 from .models import Column
@@ -9,8 +10,13 @@ from .filters import ColumnFilter
 class ColumnListView(SingleTableMixin, FilterView):
     table_class = ColumnTable
     model = Column
-    template_name = "semantic_template.html"
+    template_name = "columns.html"
     filterset_class = ColumnFilter
 
     def get_queryset(self):
         return super(ColumnListView, self).get_queryset().select_related("table")
+
+    def get_context_data(self, **kwargs):
+        ctx = super(ColumnListView, self).get_context_data(**kwargs)
+        ctx['title'] = settings.TITLE
+        return ctx
--- dbhub/settings/prod.py
+++ dbhub/settings/prod.py
@@ -1,1 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from .common import *
+
+TITLE = '会分期数据库定义'
