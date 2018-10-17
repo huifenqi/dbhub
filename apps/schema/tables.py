9a913ee56919d0823148c568ebcf0d022d5bd898
Bin <iamsk.info@gmail.com>
2018-10-17 18:37:34 +0800

    update style

--- apps/schema/tables.py
+++ apps/schema/tables.py
@@ -6,13 +6,21 @@ import django_tables2 as tables
 from models import Column
 
 
+TEMPLATE = """
+{% if record.is_deleted %}<span class="ui red label">Deleted</span>{% endif %}
+{% if record.is_comment_dirty %}<span class="ui yellow label">Not Match</span>{% endif %}
+"""
+
+
 class ColumnTable(tables.Table):
     table_comment = tables.TemplateColumn('{{ value|linebreaks }}', verbose_name='Table Comment',
                                           accessor='table.comment')
     comment = tables.TemplateColumn('{{ value|linebreaks }}')
+    warning_info = tables.TemplateColumn(TEMPLATE, verbose_name='Warning', orderable=False)
 
     class Meta:
         model = Column
-        sequence = ('name', 'table', 'data_type', 'is_null', 'default_value', 'comment', 'table_comment')
+        sequence = ('name', 'table', 'data_type', 'is_null', 'default_value', 'comment', 'table_comment',
+                    'warning_info')
         template_name = "django_tables2/semantic.html"
-        exclude = ("id",)
+        exclude = ("id", "is_comment_dirty", "is_enum", "is_deleted")
